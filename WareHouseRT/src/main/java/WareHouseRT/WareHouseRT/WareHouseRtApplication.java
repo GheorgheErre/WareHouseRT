64df473eb8fa4c2a3f7dd18b836eb88f9522c154
gheorghe.semco <gheorghe.semco@errepro.tech>
2020-09-22 17:23:02 +0200

    first push

---
+++ WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/WareHouseRtApplication.java
@@ -0,0 +1,13 @@
+package WareHouseRT.WareHouseRT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouseRtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareHouseRtApplication.class, args);
+	}
+
+}
---
+++ WareHouseRT/src/test/java/WareHouseRT/WareHouseRT/WareHouseRtApplicationTests.java
@@ -0,0 +1,13 @@
+package WareHouseRT.WareHouseRT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareHouseRtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
