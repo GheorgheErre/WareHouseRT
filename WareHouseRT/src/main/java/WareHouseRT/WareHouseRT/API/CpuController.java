576e8ebead84c5c8143997ed1e5069b1e216ede7
Gianluca <gianluca.bova@errepro.tech>
2020-09-30 12:10:38 +0200

    push backend

--- WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/API/CpuController.java
+++ WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/API/CpuController.java
@@ -1,11 +1,16 @@
 package WareHouseRT.WareHouseRT.API;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import WareHouseRT.WareHouseRT.beans.CPU;
 import WareHouseRT.WareHouseRT.service.CpuService;
@@ -21,6 +26,11 @@ public class CpuController {
 	@PostMapping("/saveCpu")
 	public void save(@RequestBody CPU cpu) {
 		service.save(cpu);
+	}
+	
+	@GetMapping("/findAllCpu")
+	public List<CPU> findAll() {
+		return service.findAll();
 	}
 
 }
--- WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/service/CpuService.java
+++ WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/service/CpuService.java
@@ -1,5 +1,7 @@
 package WareHouseRT.WareHouseRT.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +22,8 @@ public class CpuService {
 		repo.save(cpu);
 	} 
 	
+	public List<CPU> findAll() {
+		return repo.findAll();
+		
+	} 
 }
