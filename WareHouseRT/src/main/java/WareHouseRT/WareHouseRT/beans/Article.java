e086f9fb03761ee619d7f4441fab53a7d2399317
s.schiavone <s.schiavone@UTENTE051.erre.local>
2020-10-07 11:24:06 +0200

    corrected documentation

--- WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/beans/Article.java
+++ WareHouseRT/src/main/java/WareHouseRT/WareHouseRT/beans/Article.java
@@ -3,7 +3,7 @@ package WareHouseRT.WareHouseRT.beans;
 
 /**
  * 
- *  Class that represents articles that can be added to a WorkStation type Object. Contains commons data shared trough every component
+ *  Class that represents articles that can be added to a WorkStation type Object. Contains commons data shared trough every article
  *  
  *  */
 public abstract class Article extends Product{
