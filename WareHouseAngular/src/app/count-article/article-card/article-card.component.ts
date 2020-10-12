c4bc79ad4e5fb47e90a044228be457c37660a1a1
SCHIAVONE Stefano <s.schiavone@erre.local>
2020-10-12 11:35:50 +0200

    fgfg

--- WareHouseAngular/src/app/count-article/article-card/article-card.component.ts
+++ WareHouseAngular/src/app/count-article/article-card/article-card.component.ts
@@ -1,36 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { CableCountComponent } from '../cable-count/cable-count.component';
-import { CellphoneCountComponent } from '../cellphone-count/cellphone-count.component';
-import { CpuCountComponent } from '../cpu-count/cpu-count.component';
-import { DesktopCountComponent } from '../desktop-count/desktop-count.component';
-import { DockingStationCountComponent } from '../docking-station-count/docking-station-count.component';
-import { GpuCountComponent } from '../gpu-count/gpu-count.component';
-import { GraphicTabletCountComponent } from '../graphic-tablet-count/graphic-tablet-count.component';
-import { HddCountComponent } from '../hdd-count/hdd-count.component';
-import { KeyboardCountComponent } from '../keyboard-count/keyboard-count.component';
-import { LaptopCountComponent } from '../laptop-count/laptop-count.component';
-import { MonitorCountComponent } from '../monitor-count/monitor-count.component';
-import { MouseCountComponent } from '../mouse-count/mouse-count.component';
-import { OperativeSystemCountComponent } from '../operative-system-count/operative-system-count.component';
-import { PowerSupplieCountComponent } from '../power-supplie-count/power-supplie-count.component';
-import { RamCountComponent } from '../ram-count/ram-count.component';
-import { SoftwareCountComponent } from '../software-count/software-count.component';
-import { SsdCountComponent } from '../ssd-count/ssd-count.component';
-import { TokenCountComponent } from '../token-count/token-count.component';
+import { CardList } from './card-list';
 
 @Component({
   selector: 'app-article-card',
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.scss']
 })
-export class ArticleCardComponent implements OnInit {
+export class ArticleCardComponent extends CardList implements OnInit {
 
-  cards = [CableCountComponent, CellphoneCountComponent, CpuCountComponent, DesktopCountComponent, DockingStationCountComponent, 
-    GpuCountComponent, GraphicTabletCountComponent, HddCountComponent, KeyboardCountComponent, LaptopCountComponent, MonitorCountComponent
-    , MouseCountComponent, OperativeSystemCountComponent, PowerSupplieCountComponent, RamCountComponent, SoftwareCountComponent, SsdCountComponent, TokenCountComponent];
+  
   cardName: String;
 
-  constructor() { }
+  constructor() {
+    super();
+  }
   ngOnInit(): void {
   }
 
---
+++ WareHouseAngular/src/app/count-article/article-card/card-list.ts
@@ -0,0 +1,26 @@
+
+import { CableCountComponent } from '../cable-count/cable-count.component';
+import { CellphoneCountComponent } from '../cellphone-count/cellphone-count.component';
+import { CpuCountComponent } from '../cpu-count/cpu-count.component';
+import { DesktopCountComponent } from '../desktop-count/desktop-count.component';
+import { DockingStationCountComponent } from '../docking-station-count/docking-station-count.component';
+import { GpuCountComponent } from '../gpu-count/gpu-count.component';
+import { GraphicTabletCountComponent } from '../graphic-tablet-count/graphic-tablet-count.component';
+import { HddCountComponent } from '../hdd-count/hdd-count.component';
+import { KeyboardCountComponent } from '../keyboard-count/keyboard-count.component';
+import { LaptopCountComponent } from '../laptop-count/laptop-count.component';
+import { MonitorCountComponent } from '../monitor-count/monitor-count.component';
+import { MouseCountComponent } from '../mouse-count/mouse-count.component';
+import { OperativeSystemCountComponent } from '../operative-system-count/operative-system-count.component';
+import { PowerSupplieCountComponent } from '../power-supplie-count/power-supplie-count.component';
+import { RamCountComponent } from '../ram-count/ram-count.component';
+import { SoftwareCountComponent } from '../software-count/software-count.component';
+import { SsdCountComponent } from '../ssd-count/ssd-count.component';
+import { TokenCountComponent } from '../token-count/token-count.component';
+
+export class CardList {
+    cards = [CableCountComponent, CellphoneCountComponent, CpuCountComponent, DesktopCountComponent, DockingStationCountComponent, 
+        GpuCountComponent, GraphicTabletCountComponent, HddCountComponent, KeyboardCountComponent, LaptopCountComponent, MonitorCountComponent
+        , MouseCountComponent, OperativeSystemCountComponent, PowerSupplieCountComponent, RamCountComponent, SoftwareCountComponent, SsdCountComponent, TokenCountComponent];
+
+}
