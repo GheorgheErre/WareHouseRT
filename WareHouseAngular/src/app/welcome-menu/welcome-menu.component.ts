e0d0dd158dd94ceac1fc75c5d327240d221d50cd
gheorghe.semco <gheorghe.semco@errepro.tech>
2020-10-12 16:29:46 +0200

    finito welcome menu

--- WareHouseAngular/src/app/welcome-menu/welcome-menu.component.ts
+++ WareHouseAngular/src/app/welcome-menu/welcome-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faWarehouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-welcome-menu',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeMenuComponent implements OnInit {
 
+  faWarehouse = faWarehouse;
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
