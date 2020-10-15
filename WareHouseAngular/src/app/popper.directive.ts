723196ffc09f86a895284b8fe29e2c67451ba554
SCHIAVONE Stefano <s.schiavone@erre.local>
2020-10-15 11:20:54 +0200

    Popperia

--- WareHouseAngular/src/app/popper.directive.ts
+++ WareHouseAngular/src/app/popper.directive.ts
@@ -1,10 +1,40 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import Popper, { PopperOptions } from 'popper.js';
+
+
 
 @Directive({
   selector: '[appPopper]'
 })
-export class PopperDirective {
 
-  constructor() { }
+export class PopperDirective {
+  // The hint to display
+  @Input() target: HTMLElement;
+  // Its positioning (check docs for available options)
+  @Input() placement?: string;
+  // Optional hint target if you desire using other element than
+  // specified one
+  @Input() appPopper?: HTMLElement;
+
+  // The popper instance
+  private popper: Popper;
+  private readonly defaultConfig: PopperOptions = {
+    placement: 'top',
+    removeOnDestroy: true
+  };
+  constructor(private readonly el: ElementRef) { }
+  ngOnInit(): void {
+    // An element to position the hint relative to
+    const reference = this.appPopper ? this.appPopper : this.el.nativeElement;
+    this.popper = new Popper(reference, this.target, this.defaultConfig);
+  }
+  ngOnDestroy(): void {
+    if (!this.popper) {
+      return;
+    }
+
+    this.popper.destroy();
+  }
+
 
 }
