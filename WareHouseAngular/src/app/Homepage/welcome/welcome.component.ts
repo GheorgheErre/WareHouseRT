603884f43ba80e1e0c4117ca682d8aafecb914ef
gheorghe.semco <gheorghe.semco@errepro.tech>
2020-10-15 12:51:07 +0200

    Nuovo login in corso

--- WareHouseAngular/src/app/Homepage/welcome/welcome.component.ts
+++ WareHouseAngular/src/app/Homepage/welcome/welcome.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
  
+  login = false;
+  openLogin(){
+    this.login = true;
+  }
   constructor() { }
 
   ngOnInit(): void {
