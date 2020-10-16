f59885bbaf2e42431d0e25e518bc661b6536df66
gheorghe.semco <gheorghe.semco@errepro.tech>
2020-10-16 10:19:13 +0200

    login

--- WareHouseAngular/src/app/Homepage/welcome/js/hr.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function ($) {
-    setTimeout(function () {
-        $('.trans--grow').addClass('grow');
-    }, 275);
-});
---
+++ WareHouseAngular/src/app/Homepage/welcome/js/welcome.js
@@ -0,0 +1,32 @@
+//hr che si espande 
+jQuery(document).ready(function ($) {
+    setTimeout(function () {
+        $('.trans--grow').addClass('grow');
+    }, 275);
+});
+
+//Anchor con transizione
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
