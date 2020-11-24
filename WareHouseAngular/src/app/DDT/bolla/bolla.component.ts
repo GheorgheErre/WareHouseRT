1bfb1d1de99c68d60b04c85fd07e0559ae547c1a
Gianluca <gianluca.bova@errepro.tech>
2020-11-24 15:12:13 +0100

    commit bolla component

--- WareHouseAngular/src/app/DDT/bolla/bolla.component.ts
+++ WareHouseAngular/src/app/DDT/bolla/bolla.component.ts
@@ -30,10 +30,10 @@ export class BollaComponent implements OnInit {
       var pageHeight = 295;    
       var imgHeight = canvas.height * imgWidth / canvas.width;  
       var heightLeft = imgHeight;
-      const contentDataURL = canvas.toDataURL('image/png');
+      const contentDataURL = canvas.toDataURL('image/jpeg', 1);
       let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
       var position = 0;  
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.addImage(contentDataURL, 'JPEG', 0, position, imgWidth, imgHeight)  
       pdf.save('MYPdf.pdf'); // Generated PDF   
     });   
     
