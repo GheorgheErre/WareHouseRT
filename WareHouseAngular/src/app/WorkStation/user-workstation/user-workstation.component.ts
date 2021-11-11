c20671f0b24dc59c8f860e009c5a456b82a89daa
g.bova <g.bova@reply.it>
2021-11-11 16:24:18 +0100

    fix addArticleFromWareHouse

--- WareHouseAngular/src/app/WorkStation/user-workstation/user-workstation.component.ts
+++ WareHouseAngular/src/app/WorkStation/user-workstation/user-workstation.component.ts
@@ -142,9 +142,11 @@ export class UserWorkstationComponent implements OnInit {
 
     this.service.moveFromWareHouse(this.entity, this.note).subscribe(result => {
       console.log("ARTICLE SPOSTATO DA WAREHOUSE CON SUCCESSO")
-      this.updateWorkstation();
+      
       this.workstation.articles.push(this.entity);
       this.showList(this.articleType);
+
+      this.updateWorkstation();
     });   
   }
 
